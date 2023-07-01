d359a52890efe7cbb37306ed67527e81775b7922
danieldegirmenci <danieldegirmenci1@mail.com>
2023-07-01 05:18:40 +0300

    first commit

---
+++ template.js
@@ -0,0 +1,13 @@
+function darkMode() {
+    $('body').toggleClass('bg-dark bg-light');
+    $('.navbar').toggleClass('navbar-dark navbar-light');
+    $('.navbar').toggleClass('bg-dark bg-light');
+    $('body').toggleClass('text-dark text-light');
+    $('.darkToggle').toggleClass('btn-dark btn-light')
+    $('.btn').toggleClass('btn-outline-dark btn-outline-light');
+    $('h2').toggleClass('myHeading myHeading-light')
+    $('.line-clamp4').toggleClass('text-dark text-light');
+    $('h4').toggleClass('text-dark text-light');
+    
+
+  }
