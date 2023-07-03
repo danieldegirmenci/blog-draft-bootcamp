d2158f82d4ceb7ea975af8264d5dcb32b4eea9c1
danieldegirmenci <danieldegirmenci1@mail.com>
2023-07-03 17:27:12 +0300

    commit error fixed

--- template.js
+++ template.js
@@ -2,12 +2,21 @@ function darkMode() {
     $('body').toggleClass('bg-dark bg-light');
     $('.navbar').toggleClass('navbar-dark navbar-light');
     $('.navbar').toggleClass('bg-dark bg-light');
-    $('body').toggleClass('text-dark text-light');
+    $('p').toggleClass('text-dark text-light');
     $('.darkToggle').toggleClass('btn-dark btn-light')
     $('.btn').toggleClass('btn-outline-dark btn-outline-light');
-    $('h2').toggleClass('myHeading myHeading-light')
-    $('.line-clamp4').toggleClass('text-dark text-light');
-    $('h4').toggleClass('text-dark text-light');
+    $('h2').toggleClass('myHeading myHeading-light');
+    $('.card-title').toggleClass('myHeading myHeading-light')
+    // $('.line-clamp4').toggleClass('text-dark text-light');
+    // $('h4').toggleClass('text-dark text-light');
+    $(".card").toggleClass('bg-dark bg-light')
     
 
   }
+  
+  let newDate=()=>{
+    const date=new Date().getFullYear();
+
+$("#date_id").html(date);
+}
+newDate()
